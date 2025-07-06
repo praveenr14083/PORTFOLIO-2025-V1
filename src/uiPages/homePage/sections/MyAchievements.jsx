37f8ea2f582b139b98832b59d7b002a700e228ab
praveenr14083 <praveenr14083@gmail.com>
2025-07-06 17:49:43 +0530

    fix: Certificate problem and certificate hover to over overlay text implemented

--- src/uiPages/homePage/sections/MyAchievements.jsx
+++ src/uiPages/homePage/sections/MyAchievements.jsx
@@ -15,7 +15,7 @@ const certificates = [
   },
   {
     src: "/assets/certificates/web-design-dec-2024.jpeg",
-    alt: "Web Design Dec 2024",
+    alt: "Web Design December 2024",
     large: false,
   },
   {
@@ -38,16 +38,31 @@ export function MyAchievements() {
           {/* Grid Layout - Wrap Certificates */}
           <div className="grid grid-cols-1 md:grid-cols-2 place-items-center gap-5 md:gap-8 pt-8 sm:pt-10 md:pt-12 lg:pt-14">
             {certificates.map((cert, index) => (
-              <Image
+              <div
                 key={index}
-                src={cert.src}
-                alt={cert.alt}
-                width={0} // prevent fixed width
-                height={0} // prevent fixed height
-                sizes="(max-width: 768px) 100vw, 500px"
-                className={`object-contain rounded-xl z-6 hover:scale-102 transition-transform duration-300
-                      w-full md:w-[${cert.large ? "500px" : "330px"}]`}
-              />
+                className="group relative overflow-hidden rounded-xl"
+              >
+                <Image
+                  src={cert.src}
+                  alt={cert.alt}
+                  width={0}
+                  height={0}
+                  sizes="(max-width: 768px) 100vw, 500px"
+                  // className={`object-contain z-6 transition-transform duration-300 w-full md:w-[${
+                  //   cert.large ? "500px" : "330px"
+                  // }]`}
+                  className={`object-contain z-6 transition-transform duration-300 w-full ${
+                    cert.large ? "md:w-[500px]" : "md:w-[350px]"
+                  }`}
+                />
+
+                {/* Overlay */}
+                <div className="absolute inset-0 bg-c-primary/90 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  <h1 className="text-white text-lg font-semibold">
+                    {cert.alt}
+                  </h1>
+                </div>
+              </div>
             ))}
           </div>
         </div>
