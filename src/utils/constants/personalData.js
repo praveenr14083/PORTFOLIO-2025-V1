4fcb16e411cbc8fb0264d08f700b67ff33120277
praveenr14083 <praveenr14083@gmail.com>
2025-10-16 18:25:00 +0530

    email changed

--- src/utils/constants/personalData.js
+++ src/utils/constants/personalData.js
@@ -1,7 +1,7 @@
 export const PERSONAL_DATA = {
   name: "Praveen",
   phone: "+91 9442574617",
-  email: "praveen14083@gmail.com",
+  email: "praveenr14083@gmail.com",
   github: "https://github.com/praveenr14083",
   linkedin: "https://www.linkedin.com/in/praveenr14083/",
   instagram: "https://www.instagram.com/__im.__.praveen__/",
